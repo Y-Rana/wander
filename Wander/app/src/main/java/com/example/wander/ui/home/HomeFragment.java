1f51d935c04dc8111a73b1cf065d067ded5fd148
Yash Rana <yasrana@gmail.com>
2024-02-28 16:01:37 +0100

    Commented out mapbox stuff, so the app can be run on emulator.

--- Wander/app/src/main/java/com/example/wander/ui/home/HomeFragment.java
+++ Wander/app/src/main/java/com/example/wander/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package com.example.wander.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.wander.databinding.FragmentHomeBinding;
+import com.mapbox.maps.MapView;
 
 public class HomeFragment extends Fragment {
 
@@ -26,6 +30,25 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+//        //Initialize the map component
+//        final MapView map = binding.mapView;
+//
+//        //use a singleton for the ImageView. If not previously instantiated, then instantiate, otherwise
+//        //retrieve
+//
+//        //Add a touch listener to the map
+//        map.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_UP){
+//                    Log.d("map","Touch coordinates : " +
+//                            String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+//
+//                }
+//                return false;
+//            }
+//        });
         return root;
     }
 
