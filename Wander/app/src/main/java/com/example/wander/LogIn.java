6bbdeeaf22887bdb7de2216184b3cf81deb5f3f1
Nichita <n.climoc@gmail.com>
2024-03-01 10:37:46 +0100

    mainactivity is now opened first

--- Wander/app/src/main/java/com/example/wander/LogIn.java
+++ Wander/app/src/main/java/com/example/wander/LogIn.java
@@ -22,7 +22,6 @@ public class LogIn extends AppCompatActivity {
     private TextView signupRedirectText;
     private Button loginButton;
     private FirebaseAuth auth;
-    TextView forgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
