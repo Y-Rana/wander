a20aaa54ceda42b4dc0e8f57c7152ad7d29c54ed
pat-holm <patrikalexanderholm@gmail.com>
2024-03-08 09:38:27 +0100

    Added skeleton functionality of adding new groups to UI and database

--- Wander/app/src/main/java/com/example/wander/Groups.java
+++ Wander/app/src/main/java/com/example/wander/Groups.java
@@ -3,15 +3,24 @@ package com.example.wander;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,10 +32,23 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Groups extends AppCompatActivity {
 
     private TextView groups;
     private ImageView dashboardRedirectButton, searchButton;
+    private Button createButton;
+    private Button createSaveButton;
+
+    private LinearLayout groupsContainer;
+    private ScrollView groupsScrollView;
+
+    private FirebaseFirestore db;
     private FirebaseAuth auth;
     private FirebaseUser user;
     @Override
@@ -34,16 +56,21 @@ public class Groups extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        groups = findViewById(R.id.groups);
         dashboardRedirectButton = findViewById(R.id.arrow_to_dash);
         searchButton = findViewById(R.id.search);
+        createButton = findViewById(R.id.create_group);
+
+        groupsContainer = findViewById(R.id.groups_container);
+        groupsScrollView = findViewById(R.id.groups_scrollView);
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
 
         DocumentReference userGroups = db.collection("groupMembership").document(user.getDisplayName());
+
+        DocumentReference allGroups = db.collection("groupData").document("groups");
 
         userGroups.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -51,7 +78,8 @@ public class Groups extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     DocumentSnapshot groupInfo = task.getResult();
                     for (int i = 1; i <= groupInfo.getDouble("groupNum"); i++ ) {
-                        groups.append("\n"+groupInfo.getString("group" + Integer.toString(i)));
+                        addGroup(new Group(Integer.toString(i), groupInfo.getString("group" + Integer.toString(i)), Collections.singletonList("emptyAdmin")));
+                        //addGroupToFirestore(new Group(Integer.toString(i), groupInfo.getString("group" + Integer.toString(i)), null));
                     }
                 }
             }
@@ -64,5 +92,117 @@ public class Groups extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopUp();
+            }
+        });
     }
+
+    private void showPopUp() {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.create_group);
+
+        EditText editName = dialog.findViewById(R.id.editName);
+        dialog.findViewById(R.id.create_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addGroup(new Group("11", editName.getText().toString(), Collections.singletonList(user.getDisplayName())));
+                addGroupToFirestore(new Group("thisismyid", editName.getText().toString(), Arrays.asList(user.getDisplayName())));
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+    }
+
+    private void addGroup(Group group) {
+        // Create a new TextView for the group
+        TextView groupTextView = new TextView(this);
+        groupTextView.setText(group.getGroupName());
+        groupTextView.setTextSize(20);
+
+        // Requires getGroupAdmins() to not be null
+        for (int i = 0; i < group.getGroupAdmins().size(); i++) {
+            if (group.getGroupAdmins().get(i).equals(user.getDisplayName())){
+                groupTextView.setTextColor(Color.RED);
+            }
+        }
+        groupsContainer.addView(groupTextView);
+
+        // Scroll to the bottom to show the latest group
+        groupsScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                groupsScrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    private void addGroupToFirestore(Group group) {
+        // Create a new group object
+        Map<String, Object> groupHash = new HashMap<>();
+        groupHash.put("id", group.getGroupId());
+        groupHash.put("name", group.getGroupName());
+
+        // Add the group to Firestore
+        db.collection("groupData")
+                .add(groupHash)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // DocumentSnapshot addedGroup = documentReference.get();
+                        Toast.makeText(Groups.this, "Group created successfully", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Groups.this, "Error creating group", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+}
+
+class Group {
+    private String groupId;
+    private String groupName;
+    private List<String> groupAdmins;
+    private List<String> members;
+    private float[] scores;
+
+    public Group(String groupId, String groupName, List<String> groupAdmins) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+        this.groupAdmins = groupAdmins;
+    }
+
+    // Getters
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public List<String> getGroupAdmins() {
+        return groupAdmins;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    // Setters
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void setGroupAdmins(List<String> groupAdmins) {
+        this.groupAdmins = groupAdmins;
+    }
+
 }
